650ab66af77498d846efb96cc9f4ed108b0bd0e1
Abubakar Kassim <142273851+RSG0@users.noreply.github.com>
2025-08-27 16:38:23 -0500

    imported redis to improve loading image speeds
    add comments to server js to further explain what each code does

--- src/server.js
+++ src/server.js
@@ -2,18 +2,25 @@ import express from "express";
 import fetch from "node-fetch";
 import fs from "fs";
 import cors from "cors";
+import redis from "redis"
 
 const app = express();
 const PORT = 4000;
 
-app.use(cors());
+app.use(cors()); //Without this line the browser will block the server script since they contain different ports
+
+app.get("/search", (req, res) => {
+  const commanders = JSON.parse(fs.readFileSync("commanders.json"));
+  res.json(commanders);
+});
+
 
 app.get("/search", async (req, res) => {
   const pages = 426 // number of pages to fetch. //TEMPORARY
-  const seenNames = new Set();
+  const seenNames = new Set(); // should not allow duplicate games
 
   // NDJSON streaming
-  res.setHeader("Content-Type", "application/x-ndjson");
+  res.setHeader("Content-Type", "application/x-ndjson"); // Tells the client that each data will be sent as a seperate JSON object
 
   // const fileStream = fs.createWriteStream("commanders.jsonl", { flags: "a" });
 
@@ -33,14 +40,16 @@ app.get("/search", async (req, res) => {
           card.types?.includes("Creature") && // is it a creature
           card.imageUrl; // does it have an image
 
+          //Code excludes planeswalkers that are commanders. Later problem
+
         if (isLegendaryCreature && !seenNames.has(card.name)) { // if its a legendary creature and unique
-          seenNames.add(card.name);
+          seenNames.add(card.name);  //Add to Set
 
           // stream to file
           // fileStream.write(JSON.stringify(card) + "\n"); // Disabling for now
 
           // stream to frontend
-          res.write(JSON.stringify(card) + "\n");
+          res.write(JSON.stringify(card) + "\n"); //converts card data to json and sends to app.jsx
         }
       }
     } catch (err) {
